e64edf0a7da343c4642ffceca500d9d40388edec
Carlos Ramos <CarlosRamos@MacBook-Pro-de-T0reto.local>
2017-11-03 17:44:44 +0100

    New Tests

--- src/test/java/org/carlos/Buffer/BufferTest.java
+++ src/test/java/org/carlos/Buffer/BufferTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 public class BufferTest {
   private static final double DELTA = 0;
   private Buffer<Object> buffer;
+  
 
   @Before
   public void init() {
     buffer = new Buffer<Object>(0);
+    
   }
 
   @Test
@@ -25,5 +27,24 @@ public class BufferTest {
   public void checkIsBufferIsHole() throws BufferException {
     long holes = buffer.getNumberOfHoles();
     assertEquals(holes, buffer.getCapacity(), DELTA);
+  }
+  
+  @Test
+  public void checkIsBufferIsPutOperation() {
+    long holes = buffer.getNumberOfElements();
+    assertEquals(holes, buffer.getCapacity(), DELTA);
+  }
+  
+  @Test
+  public void checkIsBufferGetOperations() {
+    double holes = buffer.getNumberOfOperations();
+    assertEquals(holes, buffer.getCapacity(), DELTA);
+  }
+  
+  @Test
+  public void checkIsBufferIsObtainValue() throws BufferException {
+    buffer.putValueOfBuffer(0);
+    Object holes = buffer.getObtainValueOfBuffer();
+    assertEquals(holes, buffer.getCapacity());
   }
 }
